38cdef93d00226453bc4ad5f6b15e0e2fb6ead34
pauline1102 <hansen.pauline@yahoo.dk>
2025-06-22 20:20:26 +0200

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>
-          Heeeej
+          Heeeej med dig
         </p>
       </div>
       <p className="read-the-docs">
